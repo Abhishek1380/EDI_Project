414a57676cb39d064320fdb56ad0b187f55e7ad5
Abhishek1380 <undeabhishek707@gmail.com>
2023-05-24 11:58:22 +0530

    first commit

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -3,8 +3,9 @@ const app = express();
 const path = require("path");
 // const port = 3096;
 const hbs = require("hbs");
+const bodyParser = require("body-parser");
 
-const Register = require("./models/registers");
+const Register = require("./models/registers.js");
 
 require("./db/conn.js");
 
@@ -15,40 +16,49 @@ const port = process.env.PORT || 3080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 const publicPath = path.join(__dirname, '../public')   //for accessing comeplete file
-const template_Path = path.join(__dirname, '../template/views');
-// const partials_Path = path.join(__dirname, '../template/partials');
+const template_Path = path.join(__dirname, '../templates/views');
+const partials_Path = path.join(__dirname, '../templates/partials');
 
 //express being initialised
 
 
 app.use(express.static(publicPath))
 app.set('view engine', 'hbs')    //use to set handle bars. we need to provide key value pair
-app.set("views", template_Path);
+app.set('views', path.join(__dirname, '../public/Views'));
+hbs.registerPartials(partials_Path);
 
 
 
-app.get('', (req, res) => {
-    res.render("index");             //need to provide the hbs file name which you will find in view
-})
-
+// app.get('/', (req, res) => {
+//     res.sendFile(__dirname + "/index.html");             //need to provide the hbs file name which you will find in view
+// })
+app.get("/", function (req, res) {
+    res.render("index");
+});
 app.get("/register", (req, res) => {
     res.render("register");
 })
+
+
+app.get("/login", (req, res) => {
+    res.render("login");
+})
+
 
 app.post("/register", async (req, res) => {
     try {
         const password = req.body.password;
         const cpassword = req.body.confirmpassword;
 
-        if (password === cpassward) {
+        if (password === password) {
 
             const registerEmployee = new Register({
-                firstname: req.body.firstname,
-                lastname: req.body.lastname,
+                firstname: req.body.firstName,
+                lastname: req.body.lastName,
                 email: req.body.email,
                 phone: req.body.phone,
                 age: req.body.age,
@@ -58,6 +68,11 @@ app.post("/register", async (req, res) => {
 
             })
             const registered = await registerEmployee.save();
+            if (registered) {
+                res.json({
+                    msg: "User Registered !"
+                })
+            }
             res.status(201).render(index);
 
 
